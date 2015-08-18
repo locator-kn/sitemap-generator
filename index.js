9da604b4174fdcbf9c61612edcf661e6f9ad1984
Timo Weiß <info@timo-weiss.com>
2015-08-18 19:15:45 +0200

    refactoring, wrap fetching into promise, build sitemap

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict';
 var sm = require('sitemap');
 var cradle = require('cradle');
 
@@ -7,6 +8,8 @@ var url = 'localhost';
 var port = 5984;
 
 var smUrlRoot = 'https://locator-app.com';
+
+var relativeUrls = [];
 
 // if build is triggerd in travis
 if (process.env.travis) {
@@ -26,17 +29,45 @@ cradle.setup({
     }
 });
 
-var db = new(cradle.Connection)().database(databaseName);
+var db = new (cradle.Connection)().database(databaseName);
 
-db.view('location/locationIds', function (err, res) {
-    res.forEach(function(elem) {
-        console.log(smUrlRoot + '/location/' + elem.id);
+Promise.all([buildLocationUrls(), buildTripUrls()])
+    .then(function () {
+        var sitemap = sm.createSitemap({
+            hostname: smUrlRoot,
+            cacheTime: 600000,        // 600 sec - cache purge period
+            urls: relativeUrls
+        });
+        sitemap.toXML(function (xml) {
+            console.log(xml);
+        });
     });
-    //console.log(res, res.length);
-});
-db.view('trip/tripIds', function (err, res) {
-    res.forEach(function(elem) {
-        console.log(smUrlRoot + '/trip/' + elem.id);
+
+
+function buildLocationUrls() {
+    return new Promise(function (resolve, reject) {
+        db.view('location/locationIds', function (err, res) {
+            if (err) {
+                return reject(err);
+            }
+            res.forEach(function (elem) {
+                relativeUrls.push('/location/' + elem.id);
+            });
+            resolve();
+        });
     });
-    //console.log(res, res.length);
-});
+}
+
+function buildTripUrls() {
+    return new Promise(function (resolve, reject) {
+        db.view('trip/tripIds', function (err, res) {
+            if (err) {
+                return reject(err);
+            }
+            res.forEach(function (elem) {
+                relativeUrls.push('/trip/' + elem.id);
+            });
+            resolve();
+        });
+    });
+}

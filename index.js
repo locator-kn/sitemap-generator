6c6ed46088583a9535a596aad0bdb871ef3e7d26
Timo Weiß <info@timo-weiss.com>
2015-08-18 18:36:12 +0200

    add setup

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-/**
- * Created by timoweiss on 18/08/15.
- */
+var sm = require('sitemap');
+var cradle = require('cradle');
+
+var envVariables;
+var databaseName = 'app';
+var url = 'localhost';
+var port = 5984;
+
+// if build is triggerd in travis
+if (process.env.travis) {
+    envVariables = require('./placeholderEnv.json');
+
+} else {
+    // production
+    envVariables = require('./env.json');
+}
+
+cradle.setup({
+    host: url || 'localhost',
+    port: port || 5984,
+    auth: {
+        username: envVariables.db.COUCH_USERNAME,
+        password: envVariables.db.COUCH_PASSWORD
+    }
+});
+
+var db = new(cradle.Connection)().database(databaseName);
+console.log(db);
